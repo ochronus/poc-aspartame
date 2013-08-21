4c12fa7b086d0ba1cf5b97b93a5ce012a8e50958
Csaba Okrona <ochronus@ochronus.com>
2013-08-21 09:04:00 +0200

    initial proof of concept

---
+++ aspartame/aspartame/views.py
@@ -0,0 +1,41 @@
+from django.http import HttpResponse
+from django.utils import simplejson
+import uuid
+
+BASEDIR = 'sessions'
+
+def get_session(sid):
+    session_data = None
+    try:
+        session_file = open("{dir}/{fname}".format(dir=BASEDIR, fname=sid), 'r')
+        session_data = simplejson.loads(session_file.read())
+    except:
+        pass
+
+    return session_data
+
+def save_session(sid, session_data):
+    session_file = open("{dir}/{fname}".format(dir=BASEDIR, fname=sid), 'w')
+    session_file.write(simplejson.dumps(session_data))
+    session_file.close()
+
+
+def home(request):
+    etag = None
+    if 'HTTP_IF_NONE_MATCH' in request.META:
+        etag = request.META['HTTP_IF_NONE_MATCH']
+    else:
+        etag = uuid.uuid4()
+
+    session_data = get_session(etag)
+    if not session_data:
+        session_data = {}
+        session_data['visits'] = 1
+    else:
+        session_data['visits'] += 1
+
+    save_session(etag, session_data)
+    response = HttpResponse("Visits: {vnum}".format(vnum=session_data['visits']))
+    response['ETag'] = etag
+    response['Cache-Control'] = 'Cache-Control:private, must-revalidate, proxy-revalidate'
+    return response
